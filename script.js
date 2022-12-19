b3638cce8d48671bb42efa1b1fccbff027c0f77e
Aaron Abramson <aaron.hdcomps@gmail.com>
2022-12-19 00:03:15 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,27 +2,198 @@ let display = document.getElementById("display");
 let lastResult = document.getElementById("last-result-display");
 let buttons = Array.from(document.getElementsByClassName("calculator-button"));
 let sum;
+let lastKey;
 buttons.map( button => {
     button.addEventListener('click', (e) => {
-        switch(e.target.innerText){
+        switch(e.target.innerText)
+        {
             case 'C':
-                display.innerText = '';
+                display.innerText = "";
+                lastResult.innerHTML = display.innerText;
+                lastKey = "C";
                 break;
-            case '0':    
-                if(display.innerText == 0) {
-                    display.innerText = "";
+
+            case '0':
+                if(display.innerText == "" || lastKey == "=") {
+                    break;
+                }    
+                else{
+                    display.innerText += "0";
+                    lastKey = "0";
+                }
+                break;
+
+            case '1':
+                if(lastKey == "=") {
+                    display.innerText = "1";
+                    lastKey = "1";
+                    break;
+                }    
+                else{
+                    display.innerText += "1";
+                    lastKey = "1";}
+                break;
+
+            case '2':
+                if(lastKey == "=") {
+                    display.innerText = "2";
+                    lastKey = "2";
+                    break;
+                }    
+                else{
+                    display.innerText += "2";
+                    lastKey = "2";}
+                break;
+
+            case '3':
+                if(lastKey == "=") {
+                    display.innerText = "3";
+                    lastKey = "3";
+                    break;
+                }    
+                else{
+                    display.innerText += "3";
+                    lastKey = "3";}
+                break;
+
+            case '4':
+                if(lastKey == "=") {
+                    display.innerText = "4";
+                    lastKey = "4";
+                    break;
+                }    
+                else{
+                    display.innerText += "4";
+                    lastKey = "4";}
+                break;
+
+            case '5':
+                if(lastKey == "=") {
+                    display.innerText = "5";
+                    lastKey = "5";
+                    break;
+                }    
+                else{
+                    display.innerText += "5";
+                    lastKey = "5";}
+                break;
+            
+            case '6':
+                if(lastKey == "=") {
+                    display.innerText = "6";
+                    lastKey = "6";
+                    break;
+                }    
+                else{
+                    display.innerText += "6";
+                    lastKey = "6";}
+                break;
+
+            case '7':
+                if(lastKey == "=") {
+                    display.innerText = "7";
+                    lastKey = "7";
+                    break;
+                }    
+                else{
+                    display.innerText += "7";
+                    lastKey = "7";}
+                break;
+            
+            case '8':
+                if(lastKey == "=") {
+                    display.innerText = "8";
+                    lastKey = "8";
+                    break;
+                }    
+                else{
+                    display.innerText += "8";
+                    lastKey = "8";}
+                break;
+
+            case '9':
+                if(lastKey == "=") {
+                    display.innerText = "9";
+                    lastKey = "9";
+                    break;
+                }    
+                else{
+                    display.innerText += "9";
+                    lastKey = "9";}
+                break;
+
+            case '+':
+                if((lastKey == "+") 
+                || (lastKey == "-") 
+                || (lastKey == "*")
+                || (lastKey == "/")
+                || (display.innerText == "")) {
+                    break;
+                }    
+                else{
+                    display.innerText += "+";
+                    lastKey = "+";
+                }
+                break;
+
+            case '-':
+                if((lastKey == "+") 
+                || (lastKey == "-") 
+                || (lastKey == "*")
+                || (lastKey == "/")
+                || (display.innerText == "")) {
+                    break;
+                }    
+                else{
+                    display.innerText += "-";
+                    lastKey = "-";
+                }
+                break;
+
+            case '*':
+                if((lastKey == "+") 
+                || (lastKey == "-") 
+                || (lastKey == "*")
+                || (lastKey == "/")
+                || (display.innerText == "")) {
+                    break;
+                }    
+                else{
+                    display.innerText += "*";
+                    lastKey = "*";
+                }
+                break;
+
+            case '/':
+                if((lastKey == "+") 
+                || (lastKey == "-") 
+                || (lastKey == "*")
+                || (lastKey == "/")
+                || (display.innerText == "")) {
+                    break;
+                }    
+                else{
+                    display.innerText += "/";
+                    lastKey = "/";
+                }
+                break;
+
+            case '=':
+                if((lastKey == "+") 
+                || (lastKey == "-") 
+                || (lastKey == "*")
+                || (lastKey == "/")
+                || (display.innerText == "") 
+                || (lastKey == "=")) {
                     break;
                 }
                 else{
-                    display.innerText += "0";
-                }
-                break;
-            case '=':
                 sum = eval(display.innerText);
+                lastResult.innerHTML = display.innerText + " = " + sum;
                 display.innerText = sum;
-                lastResult = display.innerText;
-                document.getElementById("last-result-display").innerHTML = lastResult;
-                break;
+                lastKey = "=";
+                }
+                break;                
                 
             default:
                 display.innerText += e.target.innerText;
